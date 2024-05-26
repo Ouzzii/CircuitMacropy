c31e071842971bd2f5730698dbcc8ea2ccc13a21
Ouzzii <84467058+Ouzzii@users.noreply.github.com>
2024-05-26 23:39:31 +0300

    Update script.js
    Color palette function for new theme

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
+
+    eel.getColorPalette("SunsetHarmony")(function(json){
+        console.log(json)
+        const newTreeviewBackground = json[0];
+        const newFileForeground = json[1];
+        const newFileHoverBackground = json[2];
+        const newFileHoverForeground = json[3];
+        const newTreeviewTitle = json[4];
+    
+        // CSS değişkenlerini güncelleme
+        document.documentElement.style.setProperty('--treeviewBackground', newTreeviewBackground);
+        document.documentElement.style.setProperty('--fileForeground', newFileForeground);
+        document.documentElement.style.setProperty('--fileHoverBackground', newFileHoverBackground);
+        document.documentElement.style.setProperty('--fileHoverForeground', newFileHoverForeground);
+        document.documentElement.style.setProperty('--treeviewTitle', newTreeviewTitle);
+    
+    })
+
+
     eel.getSettings()(function(html){
         $('#settings').html(html)
 
