07e1454a64aa57a069acc3816b366933c8f1b9f2
Ouzzii <84467058+Ouzzii@users.noreply.github.com>
2024-05-26 23:39:01 +0300

    Update tree.js
    Added functions such as adding a new file, closing a workspace

--- assets/scripts/tree.js
+++ assets/scripts/tree.js
@@ -1,5 +1,5 @@
 function placeToTreeView(files) {
-    $('#openfolder').remove()
+    $('#openfolder').css('display', 'none')
     $('.treeview').prepend(treeTitle(files[0].path))
     $('.treeview .filesfolders').css('display', 'block')
 
@@ -7,12 +7,48 @@ function placeToTreeView(files) {
 
 
     for (let i = 0; i < files.length; i += 1) {
+        console.log(files[i].dir.split("\\").length == '2')
+        console.log(appended.includes(files[i].dir))
         if (files[i].dir == '') {
             $('.treeview .filesfolders').append(file(files[i]))
         }
+
+        else if (files[i].dir.split("/").length == '2' && !appended.includes(files[i].dir)) {
+            $('.treeview .filesfolders').append(folder(files[i]))
+            appended.push(files[i].dir)
+        }
+
         else if (files[i].dir.split("\\").length == '2' && !appended.includes(files[i].dir)) {
             $('.treeview .filesfolders').append(folder(files[i]))
             appended.push(files[i].dir)
+        }
+
+
+        else if (files[i].dir.split("/").length > '2') { // tüm alt indexler
+
+            if (!appended.includes(files[i].dir)) {
+                var dirs = files[i].dir.split('/') // mevcut olmayan klasörleri eklemek
+                var res = []
+                for (var j = 0; j <= dirs.length; j += 1) {
+                    filename = dirs[1].replace('#', 'mcrpy35mcrpy').replace('.', 'mcrpy46mcrpy')
+                    //console.log(filename)
+                    if ($('#'+filename).length == 0) {
+                        $('#'+res.join(' #')).append(subFolder(filename, j + 1))
+
+                    }
+
+                    res.push(filename)
+                    dirs.shift()
+                }
+
+                appended.push(files[i].dir)
+            }
+            if (files[i].file != '') {
+                const dir = files[i].dir.replace('#', 'mcrpy35mcrpy').replace('.', 'mcrpy46mcrpy')
+                const file = files[i].file.replace('#', 'mcrpy35mcrpy').replace('.', 'mcrpy46mcrpy')
+                $(`${dir.split('/').join(' #')}`).append(subFile(file, dir.split('/').length)) // alt dosyalar
+
+            }
         }
 
         else if (files[i].dir.split("\\").length > '2') { // tüm alt indexler
@@ -49,30 +85,58 @@ function placeToTreeView(files) {
 
 
 
-async function openFolder() {
-
+function openFolder() {
     eel.openfolder()(function(path){
-        eel.getfiles(path)(function(files){
-            placeToTreeView(files)
-        })
+        if (path != ''){
+            eel.getfiles(path)(function(files){
+                $('.treeViewTitle').remove()
+                $('.filesfolders').empty()
+                placeToTreeView(files)
+            })
+        }
     })
 }
 
+function updateFolderAsync(){
+    if (!$('#new').length){
+        eel.getWorkspaceFolder()(function(workspaceFolder){
+            if (workspaceFolder != ''){
+                eel.getfiles(workspaceFolder)(function(files){
+                    $('.treeViewTitle').remove()
+                    $('.filesfolders').empty()
+                    placeToTreeView(files)
+                })
+            }
+        })
+    }
+
+}
 
 
-$('#openfolder button').on('click', function (element) {
+$('#openfolder button').on('click', function () {
     openFolder()
+    /*var updateFolder = setInterval(function(){
+        updateFolderAsync()
+    }, 1000)*/
 })
 
 // Create file and folder structures
 function treeTitle(title) {
-    return `<h3 class='treeViewTitle'>~/${title}</h3>`
+    return `
+    <div class='treeViewTitle'>
+        <h3>~/${title}</h3>
+        <div class='icons'>
+            <img class='newfile' src='https://cdn-icons-png.flaticon.com/512/7163/7163764.png'>
+            <img class='closeWorkspace' src='https://cdn-icons-png.flaticon.com/512/12503/12503635.png'>
+        </div>
+    </div>
+    `
 }
 function folder(key) {
     return `<div class='dir closed' id='${key.dir.split('\\')[1]}'><a>~/${key.dir}</a></div>`
 }
 function file(key) {
-    return `<div class='file' id='${key.file}'><a>${key.file}</a></div>`
+    return `<div class='file' id='${key.file}'><a>${key.file}</a> <img class='dots' src="https://cdn-icons-png.flaticon.com/512/2311/2311524.png"></div>`
 }
 function subFolder(key, layer) {
     return `<div class='dir closed' style='padding-left:${layer * 10}px' id='${key}'><a>~/${key}</a></div>`
@@ -91,8 +155,9 @@ $(document).ready(function() {
 
 
 
-$('body').on('click', '.file', function (element) {
-    if (!element.currentTarget.textContent.includes('.pdf')){
+$('body').on('click', '.file a', function (element) {
+    var parentNode = $(this).parent().parent().parent()
+    if (!$(this).html().includes('.pdf')){
         if ($(`.editor ul a`).length != 0){
             var id_ = ['tabs', Array.from($(`.editor ul a`)).reverse()[0]['href'].split('-')[1]]
         
@@ -102,10 +167,10 @@ $('body').on('click', '.file', function (element) {
         }
 
 
-    if (element.currentTarget.parentNode.className == 'treeview') {
-        getFileContent(element.currentTarget.id, 'R0000T', id_)
+    if (parentNode.hasClass('treeview')) {
+        getFileContent($(this).parent().attr('id'), 'R0000T', id_)
     } else {
-        getFileContent(element.currentTarget.id, element.currentTarget.parentNode.id, id_)
+        getFileContent($(this).parent().attr('id'), parentNode.attr('id'), id_)
 
     }
     
@@ -122,12 +187,12 @@ $('body').on('click', '.file', function (element) {
         })
 
 }else{
-    if (element.currentTarget.parentNode.className == 'treeview'){
-        var path = '\\' + 'R0000T' + '\\' + element.currentTarget.id
+    if (parentNode.hasClass('treeview')){
+        var path = '\\' + 'R0000T' + '\\' + $(this).parent().attr('id')
     }else{
-        var path = '\\' + element.currentTarget.parentNode.id + '\\' + element.currentTarget.id
+        var path = '\\' + parentNode.attr('id') + '\\' + $(this).parent().attr('id')
     }
-
+    console.log(path)
     preview(path).then(()=>{
         $('#the-canvas').addClass(path)
     })
@@ -161,13 +226,13 @@ $('body').on('click', '.dir a', function(){
 
 
 async function getFileContent(file, parentdir, id) {
+    console.log(file, parent, id)
     if (parentdir!='R0000T'){
     var parent = parentdir
 }else{
     var parent = 'R0000T'
 }
     const data = eel.getcontent(parent, file)(function(fileContent){
-        console.log(fileContent)
         var content = fileContent.content
         var fullpath = fileContent.fullpath
         if (id[1] != 0){
@@ -175,12 +240,16 @@ async function getFileContent(file, parentdir, id) {
         <div id='${id[0] + '-' + (parseInt(id[1]) + 1)}' style='display: none;'>
             <textarea spellcheck='false' path='${fullpath}'>${content}</textarea>
             <div class='Derlenecek'>
-                <div>Derle ve önizle: 
-                    <input type='checkbox'>
-                </div>
                 <div class='Derle'>
                     <img class='waiting' src='https://i.gifer.com/XVo6.gif' height='50' width='50' style='visibility: hidden;'>
-                    <button>Derle</button>
+                    <select name="compileas" id="compileas">
+                        <option value="novalue" selected disabled hidden>Bir Hedef Seçin</option>
+                        <option value="latex">LaTeX</option>
+                        <option value="pdf">PDF</option>
+                    </select>
+                    <select style='display: none;' name='withcompile' id='withcompile'></select>
+                    <button id="compile">olarak derle</button>
+                    <input id="willcompile" type="checkbox">
                 </div>
             </div>
         </div>`)
@@ -189,15 +258,75 @@ async function getFileContent(file, parentdir, id) {
             <div id='${id[0] + '-' + (parseInt(id[1]) + 1)}'>
                 <textarea spellcheck='false' path='${fullpath}'>${content}</textarea>
                 <div class='Derlenecek'>
-                    <div>Derle ve önizle: 
-                        <input type='checkbox'>
-                    </div>
                     <div class='Derle'>
                         <img class='waiting' src='https://i.gifer.com/XVo6.gif' height='50' width='50' style='visibility: hidden;'>
-                        <button>Derle</button>
+                        <select name="compileas" id="compileas">
+                        <option value="novalue" selected disabled hidden>Bir Hedef Seçin</option>
+                            <option value="latex">LaTeX</option>
+                            <option value="pdf">PDF</option>
+                        </select>
+                        <select style='display: none;' name='withcompile' id='withcompile'></select>
+                        <button id="compile">olarak derle</button>
+                        <input id="willcompile" type="checkbox">
                     </div>
                 </div>
             </div>`)
         }
     })
 }
+
+
+// Yeni Dosya Oluşturma
+
+$('body').on('click', '.newfile', function(){
+    if ($('.filesfolders #new').length === 0){
+        $('.filesfolders').append(`
+            <div id="new">
+                <label>New File -></label>
+                <input type='text'>
+            </div>
+        `)
+        $('#new input').focus()
+
+    }
+})
+
+// Dosya Adı Girme Yeri Dışında Herhangi Bir Yere Tıklandığında Dosyayı Kaydet
+
+$(document).on('click', function(event){
+    if ($('.notopen').css('display') == 'none'){
+        if (!$(event.target).closest('#new').length && $('#new input').val() != '' && $('#new input').length) {
+            eel.saveFile($('#new input').val())()
+            $('#new').remove()
+        }
+    }
+})
+
+$('body').on('keydown', '#new input', function(event) {
+                // 13 is the keycode for the Enter key
+                console.log(event.keyCode)
+                if (event.keyCode === 13 && !$(event.target).closest('#new').length && $('#new input').val() != '' && $('#new input').length) {
+                    // Prevent the default action (if necessary)
+                    event.preventDefault();
+                    eel.saveFile($('#new input').val())()
+                    $('#new').remove()
+                }else if(event.keyCode === 13 && $('#new input').length){
+                    $('#new').remove()
+                }
+            });
+
+
+// Çalışma alanını kapatma
+
+$('body').on('click', '.closeWorkspace', function(){
+    $('.notopen#openfolder').css('display', 'flex')
+    $('.treeViewTitle').remove()
+    $('.filesfolders').empty()
+    $('.filesfolders').css('display', 'none')
+    eel.clearWorkspace()()
+})
+
+$('body').on('click', '.dots', function(){
+    console.log($(this))
+
+})
