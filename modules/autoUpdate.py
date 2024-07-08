b6fc4c4280da4e646fe6b67a72937e3fba4fcf21
Ouzzii <84467058+Ouzzii@users.noreply.github.com>
2024-07-08 06:02:03 +0300

    Update autoUpdate.py

--- modules/autoUpdate.py
+++ modules/autoUpdate.py
@@ -1,6 +1,10 @@
 from requests import get
 
-version = 0.1
+version = 0.2
 
 def checkUpdate(version):
-    get()
+    vars = {}
+    updated_version = get('https://raw.githubusercontent.com/Ouzzii/CircuitMacropy/main/modules/autoUpdate.py').text
+    exec(updated_version, vars)
+    print(vars['version'])
+checkUpdate(version)
