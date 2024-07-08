27e131be8d131102c70038486927c497b2c51900
Ouzzii <84467058+Ouzzii@users.noreply.github.com>
2024-07-09 00:15:50 +0300

    auto update attempt

--- modules/autoUpdate.py
+++ modules/autoUpdate.py
@@ -1,39 +1,21 @@
 from requests import get
-import pygit2
+import shutil, os, pygit2
 version = 0.1
-
-import os
-
-print(os.getcwd())
 
 def update():
     repo_url = 'https://github.com/Ouzzii/CircuitMacropy.git'
     local_dir = os.getcwd()
+    shutil.rmtree(local_dir)
+    os.mkdir(local_dir)
     repo = pygit2.clone_repository(repo_url, local_dir)
+
 def checkUpdate(version):
-    print("checking version")
+    print("versiyon kontrolü yapılıyor")
     vars = {}
     updated_version = get('https://raw.githubusercontent.com/Ouzzii/CircuitMacropy/main/modules/autoUpdate.py').text
     exec(updated_version, vars)
     if vars[version] != version:
+        print('CircuitMacropy güncelleniyor...')
         update()
     else:
-        print('CircuitMacropy is already up to date')
-        
-
-
-
-
-
-"""
-import pygit2
-
-# Klonlamak istediğiniz depo URL'si ve lokal dizin
-repo_url = 'https://github.com/username/repository.git'
-local_dir = '/path/to/save/repository'
-
-# Depoyu klonlamak için
-repo = pygit2.clone_repository(repo_url, local_dir)
-
-print(f"Depo başarıyla klonlandı: {local_dir}")
-"""
+        print('CircuitMacropy zaten güncel')
