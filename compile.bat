e5545f5b7252f5b516cb0d4f817983baf3f0f6b9
Ouzzii <84467058+Ouzzii@users.noreply.github.com>
2024-07-09 03:40:32 +0300

    deleted unimportant files

--- compile.bat
+++
@@ -1,4 +0,0 @@
-del CircuitMacropy.pyz
-cd ..
-python3.10 -m zipapp CircuitMacropy --python="C:\Users\ozank\AppData\Local\Microsoft\WindowsApps\python3.10.exe" --main=CircuitMacropy.py:main --output=CircuitMacropy.pyz
-move CircuitMacropy.pyz CircuitMacropy/CircuitMacropy.pyz
--- modules/autoUpdate.py
+++ modules/autoUpdate.py
@@ -1,5 +1,5 @@
 from requests import get
-import shutil, os, pygit2
+import shutil, os
 import zipfile
 import io
 version = 0.1
@@ -13,31 +13,23 @@ def clearDirectory(local_dir):
             shutil.rmtree(os.path.join(local_dir, content))
 
 def download_and_extract_specific_folder(repo_url, extract_to):
-    # Geçici dizin oluştur
+
     temp_dir = os.path.join(extract_to, 'temp_repo')
     if not os.path.exists(temp_dir):
         os.makedirs(temp_dir)
-
-    # GitHub API'si aracılığıyla zip dosyasının URL'sini oluşturun
     if repo_url.endswith('.git'):
         repo_url = repo_url[:-4]
     zip_url = f"{repo_url}/archive/refs/heads/main.zip"
     
-    # Zip dosyasını indirin
-    print(f"{zip_url} indiriliyor...")
     response = get(zip_url)
     response.raise_for_status()
-    
-    # Zip dosyasını belleğe yükleyin ve geçici dizine çıkartın
+
     with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
         zip_file.extractall(temp_dir)
-        print(f"İçerik geçici dizine çıkarıldı: {temp_dir}")
     
-    # İndirilen klasördeki belirli bir klasörün dosyalarını al
     extracted_folder_name = os.path.basename(repo_url) + '-main'
     specific_folder_path = os.path.join(temp_dir, extracted_folder_name)
     
-    # Belirli klasördeki dosyaları hedef dizine taşı
     for item in os.listdir(specific_folder_path):
         s = os.path.join(specific_folder_path, item)
         d = os.path.join(extract_to, item)
@@ -46,16 +38,13 @@ def download_and_extract_specific_folder(repo_url, extract_to):
         else:
             shutil.copy2(s, d)
     
-    # Geçici dizini sil
     shutil.rmtree(temp_dir)
-    print(f"Dosyalar {specific_folder_path} dizininden {extract_to} dizinine taşındı.")
 
 def update():
     repo_url = 'https://github.com/Ouzzii/CircuitMacropy.git'
     local_dir = os.getcwd()
     clearDirectory(local_dir)
     download_and_extract_specific_folder(repo_url, local_dir)
-    #pygit2.clone_repository(repo_url, local_dir)
 
 
 def checkUpdate(version):
