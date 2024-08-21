f1d10ff73d88de4609e4fbc6540cf5b45cc6c737
Ouzzii <84467058+Ouzzii@users.noreply.github.com>
2024-08-21 12:10:42 +0300

    print function changed with debug

--- modules/checkConnection.py
+++ modules/checkConnection.py
@@ -1,8 +1,11 @@
-import socket
+import socket, logging
+from modules.configuration_utils import setup_logging
+setup_logging()
+logger = logging.getLogger(__name__)
 
 def internet_connection():
     try:
-        print("İnternet bağlantısı kontrol ediliyor")
+        logger.debug("İnternet bağlantısı kontrol ediliyor")
         socket.create_connection(("8.8.8.8", 53), timeout=5)
         return True
     except (OSError, socket.timeout):
