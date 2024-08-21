34f73fd82110f6006ef3caf8e20c04bd89e0d223
Ouzzii <84467058+Ouzzii@users.noreply.github.com>
2024-08-21 12:13:37 +0300

    optimization and bug fixes for linux and windows

--- modules/createCircuitMacros.py
+++ modules/createCircuitMacros.py
@@ -2,11 +2,14 @@ import os
 from requests import get
 from bs4 import BeautifulSoup as bs
 from sys import platform
+from pathlib import Path
 from json import dump, load
 import zipfile
 
+from modules.configuration_utils import readConf, writeConf
 
 if platform != 'linux':
+    
     if os.path.exists('/'.join(__file__.split('\\')[:-2])+'\\'):
         projectPath = '/'.join(__file__.split('\\')[:-2])+'\\'
     else:
@@ -18,16 +21,8 @@ else:
         '/'.join(__file__.split('/')[:-2])+'/../'
 
 
-
-def writeConf(data):
-    with open(projectPath+'configurations.json', 'w', encoding='utf-8')as f:
-        dump(data, f, indent=2, ensure_ascii=False)
-def readConf():
-    with open(projectPath+'configurations.json', encoding='utf-8')as f:
-        return load(f)
-
 def createCircuitMacros():
-    if platform != 'linux':
+    if platform == 'win32':
         if not os.path.exists(os.environ['USERPROFILE']+'\\CMEditor'):
             os.mkdir(os.environ['USERPROFILE']+'\\CMEditor')
         if len(os.listdir(os.environ['USERPROFILE']+'\\CMEditor')) == 0:
@@ -54,16 +49,16 @@ def createCircuitMacros():
         else:
             circuitmacrospath = os.environ['USERPROFILE']+'\\CMEditor\\' + os.listdir(os.environ['USERPROFILE']+'\\CMEditor\\')[0]
     elif platform == 'linux':
-        if not os.path.exists(os.environ['ZDOTDIR']+'/CMEditor'):
-            os.mkdir(os.environ['ZDOTDIR']+'/CMEditor')
-        if len(os.listdir(os.environ['ZDOTDIR']+'/CMEditor')) == 0:
+        if not os.path.exists(os.environ['HOME']+'/CMEditor'):
+            os.mkdir(os.environ['HOME']+'/CMEditor')
+        if len(os.listdir(os.environ['HOME']+'/CMEditor')) == 0:
             circuitMacrosUrl = 'https://ece.uwaterloo.ca/~aplevich/Circuit_macros/'
             
             index = bs(get(circuitMacrosUrl).text, 'html.parser').find_all('a')
             for i in index:
                 if i['href'].startswith('Circuit_macros') and i['href'].endswith('.zip'):
                     zip = i['href']
-                    circuitmacrospath = os.environ['ZDOTDIR']+'/CMEditor/'+zip.replace('.zip', '')
+                    circuitmacrospath = os.environ['HOME']+'/CMEditor/'+zip.replace('.zip', '')
                     if not os.path.exists(circuitmacrospath):
                         os.mkdir(circuitmacrospath)
                     with open(circuitmacrospath+'\\'+zip, 'wb')as f:
@@ -78,7 +73,7 @@ def createCircuitMacros():
             #with open(circuitmacrospath+'/dpic.exe', 'wb')as f:
             #    f.write(get(dpicUrl).content)
         else:
-            circuitmacrospath = os.environ['ZDOTDIR']+'/CMEditor/' + os.listdir(os.environ['ZDOTDIR']+'/CMEditor/')[-1]
+            circuitmacrospath = os.environ['HOME']+'/CMEditor/' + os.listdir(os.environ['HOME']+'/CMEditor/')[-1]
     conf = readConf()
     if platform == 'linux':
         conf['dpicengine'] = 'dpic'
